ab07a2b5cade0035591bfd6cd8d6fa8176b29722
Dobrica Pavlinusic <dpavlin@rot13.org>
2011-01-23 15:20:27 +0100

    store mfoc output in uid.md5.log

--- dump-card.sh
+++ dump-card.sh
@@ -10,9 +10,9 @@ if [ -z "$uid" ] ; then
 	exit 1
 fi
 
-#mfoc -O $uid.mfd | tee $uid.mfoc.txt
+mfoc -O $uid.mfd | tee $uid.log
 
 md5=`md5sum 32a0ee18.mfd | cut -d" " -f1`
 mv $uid.mfd $uid.$md5.mfd
-mv $uid.mfoc.txt $uid.$md5.mfoc.txt
+mv $uid.log $uid.$md5.log
 ls -al $uid.*
